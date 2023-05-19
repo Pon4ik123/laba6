00db713fd20a117a19cfa79fbd815b568f6ce78c
Taranenko Zahar <taranenkozahar@gmail.com>
2023-05-19 18:34:06 +0200

    AGA

---
+++ cmake-build-debug/CMakeFiles/laba5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/lab/laba6/car.cpp" "CMakeFiles/laba5.dir/car.cpp.obj" "gcc" "CMakeFiles/laba5.dir/car.cpp.obj.d"
+  "E:/lab/laba6/func.cpp" "CMakeFiles/laba5.dir/func.cpp.obj" "gcc" "CMakeFiles/laba5.dir/func.cpp.obj.d"
+  "E:/lab/laba6/main.cpp" "CMakeFiles/laba5.dir/main.cpp.obj" "gcc" "CMakeFiles/laba5.dir/main.cpp.obj.d"
+  "E:/lab/laba6/person.cpp" "CMakeFiles/laba5.dir/person.cpp.obj" "gcc" "CMakeFiles/laba5.dir/person.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/laba5.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5.dir/car.cpp.obj"
+  "CMakeFiles/laba5.dir/car.cpp.obj.d"
+  "CMakeFiles/laba5.dir/func.cpp.obj"
+  "CMakeFiles/laba5.dir/func.cpp.obj.d"
+  "CMakeFiles/laba5.dir/main.cpp.obj"
+  "CMakeFiles/laba5.dir/main.cpp.obj.d"
+  "CMakeFiles/laba5.dir/person.cpp.obj"
+  "CMakeFiles/laba5.dir/person.cpp.obj.d"
+  "laba5.exe"
+  "laba5.exe.manifest"
+  "laba5.pdb"
+  "liblaba5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
